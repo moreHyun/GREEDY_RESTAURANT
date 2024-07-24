119ac606f7d52f5def723c036ed4e0360e24b6f5
manymorehyun <123722508+moreHyun@users.noreply.github.com>
2024-07-24 20:20:39 +0900

    studying JPA

---
+++ src/main/java/com/greedy/GreedyRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.greedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreedyRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreedyRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greedy/GreedyRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greedy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreedyRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
